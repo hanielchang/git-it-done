d44771b90fb02b96895cfd3818d2ba2793530af3
hanielchang <haniellloyd@gmail.com>
2021-08-12 21:01:23 -0700

    finishing issues optimization

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function (repo) {
     // add text to warning container
@@ -52,4 +68,4 @@ var getRepoIssues = function (repo) {
     console.log(repo);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
